e651f465c7a69d28d6ddf3039087b9aa4cb6a819
ISHIMWE Deborah <ishimwedebby53@gmail.com>
2025-05-04 16:10:12 +0200

    commit

--- src/main/java/rw/ac/auca/ecommerce/core/EcommerceApplication.java
+++ src/main/java/rw/ac/auca/ecommerce/core/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package rw.ac.auca.ecommerce;
+package rw.ac.auca.ecommerce.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
